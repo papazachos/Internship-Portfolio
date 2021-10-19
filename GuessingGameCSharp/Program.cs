1ccc3295ad4598ab608c79210cf2dc4819306915
Dimitris Papazacharias <dimitrispap.1995@hotmail.com>
2021-10-19 14:42:38 +0300

    Add files via upload

---
+++ GuessingGameCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessingGameCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //parameters
+            string secretWord = "Dimitris";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
+
+            while (guess != secretWord && !outOfGuesses)
+            {
+                if (guessCount < guessLimit)
+                {
+                    Console.Write("Enter a guess:");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+
+            if (outOfGuesses) {
+                Console.Write("You lose!!!"); }
+
+            else {
+                Console.Write("You win!!!");
+            }
+
+            //Displays screen for a long time 
+            Console.ReadLine();
+        }
+    }
+}
