82d6a0054e0a68cfcf33722f7ab1fad9e9ef9652
Dimitris Papazacharias <dimitrispap.1995@hotmail.com>
2021-10-21 14:26:27 +0300

    Add files via upload

---
+++ ToDoList/app.js
@@ -0,0 +1,35 @@
+document.querySelector('#push').onclick = function(){
+    //Validating Input fields
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        //Adding a New Task 
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+        //Deleting a Task
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+        //Crossing out a Completed Task 
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
